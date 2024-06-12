50158c80849aa80c3fa47733df569cab286ad63c
BigBoyGlenn <142876365+BigBoyGlenn@users.noreply.github.com>
2024-06-12 17:53:14 +0200

    Added main menu and lvl select

---
+++ Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelMenu : MonoBehaviour
+{
+    public Button[] buttons;
+    public GameObject levelButtons;
+
+    private void Awake()
+    {
+        ButtonToArray();
+        int unlockedLevel=PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for(int i=0;i<buttons.Length;i++)
+        {
+            buttons[i].interactable=false;
+        }
+        for(int i=0;i<unlockedLevel;i++)
+        {
+            buttons[i].interactable=true;
+        }
+    }
+
+    //public void OpenLevel(int levelID)
+
+    void ButtonToArray()
+    {
+        int childCount=levelButtons.transform.childCount;
+        buttons=new Button[childCount];
+        for (int i = 0; i < childCount; i++)
+        {
+            buttons[i]=levelButtons.transform.GetChild(i).gameObject.GetComponent<Button>();
+        }
+    }
+}
